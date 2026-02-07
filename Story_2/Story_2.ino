d38819a9801353dd24388a182c3274ec715171ca
Sophiatyukova-droid <sophia.tyukova@gmail.com>
2026-02-07 20:24:30 +0000

    Story_2

---
+++ Story_2/Story_2.ino
@@ -0,0 +1,19 @@
+const int RED_LED =12;
+const int GREEN_LED =11;
+void setup()
+{
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(RED_LED, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(RED_LED, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(GREEN_LED, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(GREEN_LED, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
